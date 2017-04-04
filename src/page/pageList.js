71fcb7a2d74745a0ee553a7aca4970bb88185bb8
julian <juliantaylor14@gmail.com>
2017-04-04 22:59:02 +0100

    Add a default constructor method, add handle change call back to allow fields to be editable.

--- src/page/pageList.js
+++ src/page/pageList.js
@@ -28,6 +28,26 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 }
 
 class PageList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: {
+                linkText: 'link text',
+                slug: 'slug',
+                title: 'page title',
+                coontent: 'content'
+            }
+        };
+    }
+
+    handleChange(field, event) {
+        this.setState({
+            page: {
+                field: event.target.value
+            }
+        })
+    }
+
     render() {
         let mappedPages = this.props.pages.map((page, i) =>
             <li key={i} className="asdf">{page.title}</li>
@@ -40,18 +60,29 @@ class PageList extends React.Component {
                 </ul>
                 <form onSubmit={this.props.theThing}>
                     <label htmlFor="linkText">Link test</label>
-                    <input type="text" name="linkText" id="linkText"/>
+                    <input type="text" name="linkText" id="linkText"
+                           value={this.state.page.linkText}
+                           onChange={e => { this.handleChange('linkText', e) } }
+                    />
                     <label htmlFor="slug">Slug</label>
-                    <input type="text" name="slug" id="slug" />
+                    <input type="text" name="slug" id="slug"
+                           value={this.state.page.slug}
+                           onChange={e => { this.handleChange('slug', e) } }
+                    />
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" id="title"/>
+                    <input type="text" name="title" id="title"
+                           value={this.state.page.title}
+                           onChange={e => { this.handleChange('title', e) } }
+                    />
                     <TinyMCE
                         content="<p>This is the initial content of the editor</p>"
                         config={{
                             plugins: 'link image code',
                             toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
                         }}
+                        value={this.state.page.content}
                         id="content"
+                        onChange={e => { this.handleChange('content', e) } }
                     />
                     <button>Click me</button>
                 </form>
